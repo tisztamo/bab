eda1a4b5902f210b70f618a0ac1e4c9713386077
Schäffer Krisztián <schaffer.krisztian@gmail.com>
2019-08-01 16:13:47 +0200

    Switched to pixi-legacy

--- src/elements/ground.js
+++ src/elements/ground.js
@@ -10,7 +10,7 @@ export class Ground extends GameElement {
   createBody() {
     const groundBD = new b2.b2BodyDef()
     const shape0 = new b2.b2EdgeShape()
-    shape0.Set(new b2.b2Vec2(0, this.simulation.height - 0.7),
+    shape0.Set(new b2.b2Vec2(0, this.simulation.height - 0.2),
       new b2.b2Vec2(this.simulation.width * 0.7, this.simulation.height - 0.2))
     this.body = this.physicsWorld.CreateBody(groundBD)
     this.body.CreateFixture(shape0, 0.0)
